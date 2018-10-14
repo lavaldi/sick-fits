641f0889da6cddcdda39693601031c81af6237ba
Claudia Valdivieso <claumavaldivieso@gmail.com>
2018-10-13 19:46:15 -0500

    first commit

---
+++ frontend/components/Meta.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Meta = () => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+      <link rel="shortcut icon" href="/static/favicon.png" />
+      <link rel="stylesheet" href="/static/nprogress.css" />
+      <title>Sick Fits!</title>
+    </Head>
+  )
+}
+
+export default Meta
